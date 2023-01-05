8d6578e5b1b9bde5dee5cad97dac83286d0f96b2
houman ebrahimi <houmanebrahimi67@gmail.com>
2023-01-05 13:39:50 -0500

    added more features

--- src/Pages/search.js
+++ src/Pages/search.js
@@ -8,6 +8,7 @@ const url = 'http://localhost:8000/teslas'
 
 const Searching = ({carData,removeCars,favoriteCars,setDcarData}) => {
   const [a,b]=useState('')
+  const [c,d]=useState('')
 
   const fetchCars = async () => {    
 
@@ -22,31 +23,54 @@ const Searching = ({carData,removeCars,favoriteCars,setDcarData}) => {
     }
 
 }
+
+const fetchBrands = (Brand) =>{
+  const carData=
+}
   const handleChange =(e)=> {
     b(e.target.value);
+    d(e.target.value)
   };
 
   const handleSearchChange = (e)=>{
       if(a.trim().length!==0){
         
-        const filterCars=carData.filter(data=>data.Brand.includes(a))
+        const filterCars=carData.filter(data=>data.Brand.includes(a) || data.Brand.toString().toUpperCase()===a);
         setDcarData(filterCars);
 
       }
       else{
         fetchCars();
         console.log("empty bro")
-      }
+      }     
+}
+const handleBrand = (e)=>{
+  var brand=0;
+  if(c.trim().length!==0){
+    
+    const filterCars=carData.filter(data=>data.title.includes(c)|| data.title.toString().toUpperCase()===c)
+    setDcarData(filterCars);
 
+    if(a.trim().length!==0){
+      brand=a;
+    }
 
-      
+  }
+  else{
+    fetchBrands(a);
+    console.log("empty bro")
+  }     
 }
     return (
       <div >
         
           <input onChange={handleChange} placeholder="Search" type="text" id="search">
           </input>
-        <button onClick={handleSearchChange}> Filter </button>
+        <button onClick={handleSearchChange}> Brand</button>
+      <p> &nbsp;</p>
+        <input onChange={handleChange} placeholder="Search" type="text" id="search">
+          </input>
+        <button onClick={handleBrand}> Model </button>
         <h2 className="background">
             Available Cars
         </h2>
